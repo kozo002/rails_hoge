7493c794e185ecdb32af41acd0c0f0065bc86f58
J.Fukaya <fukajun.shark@gmail.com>
2016-11-07 20:11:25 +0900

    Add user model and hoge spec

--- test/rails_hoge_test.rb
+++ test/rails_hoge_test.rb
@@ -4,4 +4,8 @@ class RailsHoge::Test < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, RailsHoge
   end
+
+  test "hoge method return hoge" do
+    assert_equal 'hoge', User.hoge
+  end
 end
